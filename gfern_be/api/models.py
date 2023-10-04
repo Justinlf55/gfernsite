69cd3947eb4dc8f10343fe55699bf3604d306a6a
Justinlf55 <52366682+Justinlf55@users.noreply.github.com>
2023-10-04 10:54:17 -0700

    edited User, logout working, cleanup imports

--- gfern_be/api/models.py
+++ gfern_be/api/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Admin(models.Model): 
-    user_name = models.CharField(max_length=12, default="", unique=True)
-    first_name = models.CharField(max_length=12, default="", unique=True)
-    last_name = models.CharField(max_length=24, default="", unique=True)
-    user_name = models.CharField(max_length=12, default="", unique=True)
-
+class User(models.Model): 
+    user_name = models.CharField(max_length=12, default="", unique=True, null=False)
+    first_name = models.CharField(max_length=12, default="", unique=True, null=False)
+    last_name = models.CharField(max_length=24, default="", unique=True, null=False)
+    user_name = models.CharField(max_length=12, default="", unique=True, null=False)
+    email_address = models.EmailField(verbose_name="email", max_length=60, unique=True, blank=True, null=True, default=None)
+    password = models.CharField(max_length=100, default="", null=False)
+    date_of_birth = models.CharField(max_length=30, blank=True, null=True, default=None)
+    zipcode = models.CharField(max_length=10, blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
--- gfern_be/api/views.py
+++ gfern_be/api/views.py
@@ -1,19 +1,22 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response 
-
-from .serializers import UserSerializer
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.response import Response 
+from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token 
 from django.contrib.auth.models import User 
-
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import logout
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def main(request):
     return Response("Home")
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
     user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
@@ -23,8 +26,8 @@ def login(request):
     return Response({"token": token.key, "user": serializer.data})
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def signup(request):
-    print(request)
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -35,12 +38,18 @@ def signup(request):
         return Response({"token": token.key, "user": serializer.data})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
     return Response("passed for {}".format(request.user.email))
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def logout(request):
+
+    request.user.auth_token.delete()
+
+    logout(request._request)
+
+    return Response('User Logged out successfully')
